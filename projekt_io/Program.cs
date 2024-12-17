f73d603edc04b3c82bd8ab40faf64a0623325bfa
Jakub <habdasjakub@gmail.com>
2024-12-17 12:26:40 +0100

    Dodanie klasy student

--- projekt_io/Program.cs
+++ projekt_io/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using projekt_io;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Podaj imie: ");
 string imie = Console.ReadLine();
 Console.WriteLine("Witaj " + imie);
+
+student Student1 = new student("Tomasz", "Kowalski");
+Console.WriteLine("imie i nazwisko studenta: " + Student1.imie + Student1.nazwisko);
---
+++ projekt_io/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_io
+{
+    internal class student
+    {
+        public string imie {  get; set; }
+        public string nazwisko { get; set; }
+
+        public student(string name, string surname)
+        {
+            this.imie = name;
+            this.nazwisko = surname;
+        }
+    }
+}
